3dbbce58c4c3193202bb22df4b4fa8afb549eae4
hanmant4615 <hanmantchavan2403@gmail.com>
2024-11-15 22:03:25 +0530

    Added css and new feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Gallery from "./Gallery";
 import "./App.css";
-import { url } from "./utils";
+import { blur, grayscale, url } from "./utils";
 
 function App() {
   const [photoGalleryArray, updatePhotoGalleryArray] = useState([]);
 
+  const handleClickbtn1 = () => {
+    axios
+      .get(grayscale)
+      .then((response) => console.log(response.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleClickbtn2 = () => {
+    axios
+      .get(blur)
+      .then((response) => console.log(response.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   useEffect(() => {
     axios
       .get(url)
@@ -22,6 +39,14 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
         <div className="w-100 text-light">Image Gallery</div>
+        <div className="button-container">
+          <button className="btn" onClick={() => handleClickbtn1}>
+            Random grayscale
+          </button>
+          <button className="btn" onClick={() => handleClickbtn2}>
+            Random blur
+          </button>
+        </div>
       </nav>
       <div className="row">
         <Gallery images={photoGalleryArray} />
